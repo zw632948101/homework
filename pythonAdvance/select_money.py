a7222ef17c3724b5e343000679361cdc65fe3b76
张伟 <wei.zhang@worldfarm.com>
2021-02-23 20:45:15 +0800

    python脚本进阶工资案例

--- pythonAdvance/select_money.py
+++ pythonAdvance/select_money.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 # ! _*_ coding:utf-8 _*_
 __author__ = 'wei.zhang'
-
+"""
+定义工资查询模块 select_money，用来展示工资数额
+"""
 from money import salary_money
 
 
 def select_money():
+    """
+    调用存款变量，并展示
+    :return:当前存款
+    """
     print(f"当前工资：{salary_money}")
     return salary_money
+
 
 if __name__ == '__main__':
     select_money()
--- pythonAdvance/send_money.py
+++ pythonAdvance/send_money.py
@@ -9,6 +9,10 @@ from money import saved_money, salary_money
 
 
 def send_money():
+    """
+    调用工资变量和存款变量计算发工资后的存款金额
+    :return: 发工资后的存款金额
+    """
     print(f"未发工资存款为{saved_money}.")
     money = saved_money + salary_money
     print(f"发完工资后存款为{money}")
